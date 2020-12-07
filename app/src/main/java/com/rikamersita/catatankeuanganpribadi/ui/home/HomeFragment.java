8a3413937d5835cecd6c7b94bc748009cd516af2
RikaMersita <rikamersita@gmail.com>
2020-12-07 13:09:24 +0700

    TA

--- app/src/main/java/com/rikamersita/catatankeuanganpribadi/ui/home/HomeFragment.java
+++ app/src/main/java/com/rikamersita/catatankeuanganpribadi/ui/home/HomeFragment.java
@@ -26,14 +26,23 @@ public class HomeFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            TextView tanggal, jumlah, keterangan;
+           // TextView tanggal, jumlah, keterangan;
 
 
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
+/*
+                tanggal = (TextView) textView.findViewById(R.id.tv_tanggal);
+                jumlah = (TextView) textView.findViewById(R.id.tv_jumlah);
+                keterangan = (TextView) textView.findViewById(R.id.tv_keterangan);
 
-                
+                String tgl = getActivity().getIntent().getExtras().getString("tanggal");
+                String jml = getActivity().getIntent().getExtras().getString("jumlah");
+                String ket = getActivity().getIntent().getExtras().getString("keterangan");
+
+
+ */
 
 
             }
